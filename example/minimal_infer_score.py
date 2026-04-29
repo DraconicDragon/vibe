3c8d10c57663e2358b4ba35ffbb864d0069eaea0
draconicdragon <altdragon@outlook.de>
2026-04-29 23:06:16 +0200

    refactor: change score model minimal infer example

--- example/minimal_infer_score.py
+++ example/minimal_infer_score.py
@@ -11,13 +11,15 @@ logging.basicConfig(
 )
 logging.getLogger("vibe").setLevel(logging.WARNING)
 
+MODEL_SOURCE = "/mnt/T7/Projects/GitHub/vibe/models/deepghs/anime_aesthetic/swinv2pv3_v0_448_ls0.2_x"
 
 with vibe.load(
-    "waifu-scorer-v3",
-    # auto_download=False,
+    "dghs-aes-swinv2pv3-448-ls0.2-x",
+    source=MODEL_SOURCE,
 ) as session:
     result = session.infer(
         Image.open("example/example.jpg"),
     ).first()
 
-    print(f"score: {result.score:.3f}")
+    print(result.scores)
+    print(result.metrics)
